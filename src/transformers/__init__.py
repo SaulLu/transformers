7ea24543bd48fb29db105044281386aaa0596796
Niels Rogge <niels.rogge1@gmail.com>
2021-11-22 17:16:46 +0100

    Remove MarkupLMForMaskedLM

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1083,8 +1083,6 @@ if is_torch_available():
     _import_structure["models.markuplm"].extend(
         [
             "MARKUPLM_PRETRAINED_MODEL_ARCHIVE_LIST",
-            "MarkupLMForCausalLM",
-            "MarkupLMForMaskedLM",
             "MarkupLMForMultipleChoice",
             "MarkupLMForQuestionAnswering",
             "MarkupLMForSequenceClassification",
@@ -3012,8 +3010,6 @@ if TYPE_CHECKING:
         from .models.marian import MarianForCausalLM, MarianModel, MarianMTModel
         from .models.markuplm import (
             MARKUPLM_PRETRAINED_MODEL_ARCHIVE_LIST,
-            MarkupLMForCausalLM,
-            MarkupLMForMaskedLM,
             MarkupLMForMultipleChoice,
             MarkupLMForQuestionAnswering,
             MarkupLMForSequenceClassification,
